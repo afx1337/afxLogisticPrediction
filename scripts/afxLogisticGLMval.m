6b86036d0f8288359350fdef2b451c8a06b690dc
alexistmeinname <74628004+alexistmeinname@users.noreply.github.com>
2023-05-23 14:09:28 +0200

    Add files via upload

--- scripts/afxLogisticGLMval.m
+++ scripts/afxLogisticGLMval.m
@@ -8,14 +8,22 @@ function [yfit] = afxLogisticGLMval(betas,x,scale)
     % predicts responses based using logistic GLM
     % yfit  ... predicted responses (observations x voxels)
     
+     % reshape input data (x)  and clear x (for freeing memory)
+    x2 = reshape(permute(x,[2 1 3]),size(x,2),size(x,1)*size(x,3));
+    clear x;
+    x2 = x2';
+    xnan = any(isnan(x2),2);
+    
+    % delete observations with NaNs
+    x2(xnan,:) = [];
+    
     % scale x
     if ~isempty(scale)
-        x = (x-scale.mean)./scale.std;
+        x2 = (x2-scale.mean)./scale.std;
     end
+    
     % initialize yfit
-    yfit = nan(size(x,1),size(x,3));
-    % predict response for every voxel
-    for iVoxel = 1:size(x,3)
-        yfit(:,iVoxel) = glmval(betas(:,iVoxel),x(:,:,iVoxel),'logit');
-    end
+    yfit = nan(size(x2,1),1);
+    % predict response
+    yfit = glmval(betas,x2,'logit');
 end
